dc6c054a72f142e5d843a96c490f3485e9b21be0
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-24 19:14:01 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -14,6 +14,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
+    elsif req.path.match(/add/)
     else
       resp.write "Path Not Found"
     end
