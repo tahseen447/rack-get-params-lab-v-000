3b6df0fdd0ab0d8ab69aa71203e8b2f4cdb54ffe
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-24 19:30:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -18,7 +18,7 @@ class Application
       if @@cart.empty?
       resp.write("Your cart is empty")
     else
-      @@cart.each {|item| resp.write("#{item\n}")}
+      @@cart.each {|item| resp.write("#{item}\n")}
     end
     elsif req.path.match(/add/)
       item = req.params['a']
