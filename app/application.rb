f830f39c53eed8b058c5459e7385b5abce539faf
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-08-24 19:32:35 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -39,7 +39,7 @@ class Application
   end
 
   def handle_add(item)
-    if @@items.include(item)
+    if @@items.include?(item)
       return "added #{item}"
     else
       return "We don't have that item"
